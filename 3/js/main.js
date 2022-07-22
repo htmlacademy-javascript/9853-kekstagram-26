13adbc96f8f40ad47c3ae0e302b2d962880c31cc
Keks <keks@htmlacademy.ru>
2022-07-23 00:51:57 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,13 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt(1, 9);
+
+function validateLength(value, max) {
+  return value.length <= max;
+}
+validateLength('text', 140);
+
