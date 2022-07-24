41a2d78afacfc71a9daab5021b7aacf7501246c0
Maxim Nikolyukin <Ch1ppyone@mail.ru>
2022-07-24 13:36:48 +0300

    7.16. Отрисуй меня полностью

--- js/render.js
+++ js/render.js
@@ -1,22 +1,29 @@
-import {generatePhotos} from './data.js';
-
 const pictureBlock = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-function renderPhotos() {
+const renderPosts = (posts) => {
   const pictures = document.createDocumentFragment();
-  const photos = generatePhotos();
-  for (let i = 0; i < photos.length; i++) {
-    const picture = pictureTemplate.cloneNode(true);
-    picture.querySelector('.picture__img').src = photos[i].url;
-    picture.querySelector('.picture__likes').textContent = photos[i].likes;
-    picture.querySelector('.picture__comments').textContent = photos[i].comments.length;
 
+  posts.forEach((post) => {
+    const picture = pictureTemplate.cloneNode(true);
+    picture.querySelector('.picture__img').src = post.url;
+    picture.querySelector('.picture__likes').textContent = post.likes;
+    picture.querySelector('.picture__comments').textContent = post.comments.length;
+
+    picture.dataset.postId = post.id;
     pictures.appendChild(picture);
-  }
+  });
 
   pictureBlock.appendChild(pictures);
-}
+};
 
-export {renderPhotos};
+const bindPostClickEvent = (callback) => {
+  pictureBlock.addEventListener('click', (e) => {
+    const picture = e.target.closest('.picture');
+    if (picture) {
+      callback(picture.dataset.postId);
+    }
+  });
+};
 
+export {renderPosts, bindPostClickEvent};
