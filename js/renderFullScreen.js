6e753f4dfb466184e453dc73e69716a589999fd8
Maxim Nikolyukin <Ch1ppyone@mail.ru>
2022-07-25 23:14:54 +0300

    Правда или действие (часть 2)

--- js/renderFullScreen.js
+++ js/renderFullScreen.js
@@ -1,16 +1,56 @@
+const SHOWN_COMMENTS_AMOUNT = 5;
+
 const fullScreenPicture = document.querySelector('.big-picture');
 const socialComments = fullScreenPicture.querySelector('.social__comments');
+const socialCommentCount = fullScreenPicture.querySelector('.social__comment-count');
+const uploadComments = fullScreenPicture.querySelector('.social__comments-loader');
 const closeButton = fullScreenPicture.querySelector('.big-picture__cancel');
 
-const close = () => {
-  fullScreenPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-
-  closeButton.removeEventListener('click', close);
-  window.removeEventListener('keydown', close);
+const getCommentsAmount = (value) => {
+  socialCommentCount.childNodes[0].textContent = `${value} из `;
 };
 
-const showComments = (comments) => {
+const hideExtraComments = (comments) => {
+  const extraComments = comments.slice(SHOWN_COMMENTS_AMOUNT);
+  extraComments.forEach((extraComment) => {
+    extraComment.classList.add('hidden');
+  });
+};
+
+const getMoreComments = (comments) => {
+  const commentsAmount = comments.length;
+
+  if(commentsAmount <= SHOWN_COMMENTS_AMOUNT) {
+    getCommentsAmount(commentsAmount);
+    uploadComments.classList.add('hidden');
+    return;
+  }
+
+  getCommentsAmount(SHOWN_COMMENTS_AMOUNT);
+
+  let shownCommentsAmount = SHOWN_COMMENTS_AMOUNT;
+  return function() {
+    const availableAmount = commentsAmount - shownCommentsAmount;
+    const newCommentsAmount = shownCommentsAmount + Math.min(SHOWN_COMMENTS_AMOUNT, availableAmount);
+
+    const commentsToShow = comments.slice(shownCommentsAmount, newCommentsAmount);
+    commentsToShow.forEach((comment) => {
+      comment.classList.remove('hidden');
+    });
+
+    if (availableAmount <= SHOWN_COMMENTS_AMOUNT) {
+      shownCommentsAmount += availableAmount;
+      getCommentsAmount(shownCommentsAmount);
+      uploadComments.classList.add('hidden');
+      return;
+    }
+
+    shownCommentsAmount += SHOWN_COMMENTS_AMOUNT;
+    getCommentsAmount(shownCommentsAmount);
+  };
+};
+
+const showAllComments = (comments) => {
   let allComments = '';
   comments.forEach((comment) => {
     allComments +=
@@ -26,6 +66,14 @@ const showComments = (comments) => {
   socialComments.innerHTML = allComments;
 };
 
+const close = () => {
+  fullScreenPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  closeButton.removeEventListener('click', close);
+  window.removeEventListener('keydown', close);
+};
+
 const escapeKeydown = (evt) => {
   if(evt.key === 'Escape'){
     close();
@@ -39,13 +87,15 @@ const renderFullScreen = (post) => {
   fullScreenPicture.querySelector('.likes-count').textContent = post.likes;
   fullScreenPicture.querySelector('.comments-count').textContent = post.comments.length;
   fullScreenPicture.querySelector('.social__caption').textContent = post.description;
-  fullScreenPicture.querySelector('.social__comment-count').classList.add('hidden');
-  fullScreenPicture.querySelector('.comments-loader').classList.add('hidden');
 
-  showComments(post.comments);
+  showAllComments(post.comments);
+
+  const allComments = Array.from(socialComments.querySelectorAll('li'));
+  hideExtraComments(allComments);
+  uploadComments.onclick = getMoreComments(allComments);
 
   closeButton.addEventListener('click', close);
-  window.addEventListener('keydown', escapeKeydown );
+  window.addEventListener('keydown', escapeKeydown);
 };
 
 export {renderFullScreen};
